6bf5252b2ab0c7713e0225dab1631119f7a937a6
Amitron-Dev <martingrellier79@gmail.com>
2025-03-16 13:59:05 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+let tours = 0
+input.onButtonPressed(Button.A, function () {
+    tours = tours + 1
+    basic.showNumber(tours)
+})
+input.onButtonPressed(Button.AB, function () {
+    tours = 0
+    basic.showNumber(tours)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < tours; index++) {
+        for (let index = 0; index < 4; index++) {
+            basic.showNumber(tours)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 39)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 39)
+            basic.pause(2250)
+            Kitronik_Move_Motor.stop()
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            if (tours == tours - 0) {
+                Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 39)
+                basic.pause(540)
+                Kitronik_Move_Motor.stop()
+            }
+        }
+    }
+})
