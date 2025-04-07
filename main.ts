2d97fbf00cb7f640efb4860ebd13cabd8a288c08
Amitron-Dev <martingrellier79@gmail.com>
2025-04-07 08:53:36 +0200

    changement d'action pour demarrage

--- main.ts
+++ main.ts
@@ -6,17 +6,20 @@ input.onButtonPressed(Button.AB, function () {
     tours = 0
     basic.showNumber(tours)
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < tours; index++) {
         for (let index = 0; index < 4; index++) {
             basic.showNumber(tours)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 60)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 60)
-            basic.pause(1000)
+            basic.pause(600)
             Kitronik_Move_Motor.stop()
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 40)
-            basic.pause(500)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 35)
+            basic.pause(50)
         }
     }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
 })
 let tours = 0
